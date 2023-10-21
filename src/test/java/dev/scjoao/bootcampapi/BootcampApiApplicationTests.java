f389fe0d7fbb965c58e249dedfa1f2a2237e319e
João Carlos <joao.carlos_25@hotmail.com>
2023-10-21 17:05:13 -0300

    Initial commit

---
+++ src/test/java/dev/scjoao/bootcampapi/BootcampApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.scjoao.bootcampapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
