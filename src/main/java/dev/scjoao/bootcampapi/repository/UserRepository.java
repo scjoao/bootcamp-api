c89433456194c196df3c6b6a0a635824fad82e93
João Carlos <joao.carlos_25@hotmail.com>
2023-10-21 18:08:19 -0300

    Implementacao da camada de dominio

---
+++ src/main/java/dev/scjoao/bootcampapi/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package dev.scjoao.bootcampapi.repository;
+
+import dev.scjoao.bootcampapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+}
